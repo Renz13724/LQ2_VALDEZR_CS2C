ffed7fb4095a132f33ec6974a9de594292b00dc1
VALDEZ R. <renzv753@gmail.com>
2024-10-05 18:10:18 +0800

    Create Code_Transformation3.js

---
+++ Code_Transformation3.js
@@ -0,0 +1,9 @@
+let fname = "Renz";
+let i = 0;
+while (fname.length > i) {
+  let c = 2 + i;
+  let nLengthCounterTwo = fname.length - c;
+  console.log(fname);
+  console.log("Iterations Name was Printed: " + i);
+  i++;
+}
